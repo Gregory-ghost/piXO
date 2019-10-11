6801578eeb921a042195188df86022dac29204ef
Gregory-ghost <43817932+Gregory-ghost@users.noreply.github.com>
2019-10-11 11:54:01 +0300

    Add files via upload

--- pi21/api/application/user/User.php
+++ pi21/api/application/user/User.php
@@ -1,1 +1,17 @@
+<?php
 
+class User {
+
+    public function login($login, $password) {
+        // get user from DB
+        if ($login === 'vasya' && $password === '123') {
+            $token = md5(rand() * 100000);
+            return array('token' => $token);
+        }
+        return false;
+    }
+
+    public function logout() {}
+
+    public function registration() {}
+}

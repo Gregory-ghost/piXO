1e6d16fd1eb51aef11e77da6c3aad968ee01ac25
Gregory-ghost <43817932+Gregory-ghost@users.noreply.github.com>
2019-10-11 11:50:27 +0300

    Add files via upload

--- pi21/api/index.php
+++ pi21/api/index.php
@@ -1,1 +1,37 @@
+<?php
+error_reporting(1);
 
+require_once('application/Application.php');
+
+function router($params) {
+    $method = $params['method'];
+    if ($method) {
+        $app = new Application();
+        switch ($method) {
+            // about user
+            case 'login': return $app->login($params);
+            // about game
+            case 'turn': return $app->turn($params);
+            default: return false;
+        }
+    }
+    return false;
+}
+
+function answer($data) {
+    if ($data) {
+        return array(
+            'result' => 'ok',
+            'data' => $data
+        );
+    }
+    return array(
+        'result' => 'error',
+        'error' => array(
+            'code' => 9000,
+            'text' => 'unknown error'
+        )
+    );
+}
+
+echo json_encode(answer(router($_GET)));

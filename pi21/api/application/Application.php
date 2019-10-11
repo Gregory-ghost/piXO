5d544b8cfdb7a8e3364dd3f52f86eee7b4cf07a2
Gregory-ghost <43817932+Gregory-ghost@users.noreply.github.com>
2019-10-11 11:51:11 +0300

    Add files via upload

--- pi21/api/application/Application.php
+++ pi21/api/application/Application.php
@@ -1,1 +1,32 @@
+<?php
 
+require_once('user/User.php');
+require_once('piXO/PiXO.php');
+
+class Application {
+
+    function __construct() {
+        $this->user = new User();
+        $this->piXO = new PiXO();
+    }
+
+    /**************/
+    /* ABOUT USER */
+    /**************/
+    public function login($params) {
+        if ($params['login'] && $params['password']) {
+            return $this->user->login($params['login'], $params['password']);
+        }
+        return false;
+    }
+
+    /**************/
+    /* ABOUT GAME */
+    /**************/
+    public function turn($params) {
+        if ($params['id'] && $params['x'] && $params['y']) {
+            return $this->piXO->turn($params['id'], $params['x'], $params['y']);
+        }
+        return false;
+    }
+}
